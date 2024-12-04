f7a71bd48f60b0f9cf7da5e3ca7ad4d9e183e366
Laly <laly.teissier@gmail.com>
2024-12-04 14:33:10 +0100

    change arduino into micropython

--- step1/step1.ino
+++ step1/step1.ino
@@ -8,18 +8,20 @@ const int echo_pin = 18;
 const int sound_sensor_pin = 34;
 const int led_pin = 23;
 
-// Paramètres WebSocket
-const char* websocket_host = "192.168.1.100"; // Remplacez par l'IP de votre Mac
-const int websocket_port = 81; // Port de votre serveur WebSocket
-
 // Vitesse du son
 #define SOUND_SPEED 340
 #define TRIG_PULSE_DURATION_US 10
-#define SOUND_THRESHOLD 300
+#define SOUND_THRESHOLD 200
 
 // Objets
 WebSocketsClient webSocket;
 StaticJsonDocument<200> jsonDoc;
+
+// Paramètres WebSocket
+const char* websocket_host = "192.168.10.250:"; // Remplacez par l'IP de votre Mac
+const int websocket_port = 8080; // Port de votre serveur WebSocket
+const char* websocket_path = "/say"; // route de connexion pour les ws
+
 
 long ultrason_duration;
 float distance_cm;
@@ -30,21 +32,7 @@ const unsigned long sendInterval = 1000; // Envoyer des données toutes les seco
 const char* ssid = "Cudy-EFFC";
 const char* password = "33954721";
 
-// Callback WebSocket
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch(type) {
-    case WStype_DISCONNECTED:
-      Serial.println("WebSocket déconnecté");
-      break;
-    case WStype_CONNECTED:
-      Serial.println("WebSocket connecté");
-      break;
-    case WStype_TEXT:
-      Serial.print("Message reçu: ");
-      Serial.println((char*)payload);
-      break;
-  }
-}
+
 
 void setup() {
   Serial.begin(115200);
@@ -52,7 +40,6 @@ void setup() {
   // Configuration WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(100);
@@ -69,8 +56,30 @@ void setup() {
   pinMode(led_pin, OUTPUT);
 
   // Configuration WebSocket
-  webSocket.begin(websocket_host, websocket_port, "/");
+  webSocket.begin(websocket_host, websocket_port, websocket_path);
+  Serial.println("websocket :");
+  Serial.print(websocket_host);
+  Serial.print(websocket_port);
+  Serial.print(websocket_path);
   webSocket.onEvent(webSocketEvent);
+}
+
+// Callback WebSocket
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_CONNECTED:
+      Serial.println("WebSocket connecté");
+      // send message to server when Connected
+			webSocket.sendTXT("Connected");
+      break;
+    case WStype_DISCONNECTED:
+      Serial.println("WebSocket déconnecté");
+      break;
+    case WStype_TEXT:
+      Serial.print("Message reçu: ");
+      Serial.println((char*)payload);
+      break;
+  }
 }
 
 void loop() {
